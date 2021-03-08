3278ace3c315be285115eb58814bfc3ca1ea3baf
THIRTHAPRASAD <prasadprabha19@gmail.com>
2021-03-08 13:22:02 +0530

    added files

---
+++ provider.tf
@@ -0,0 +1,8 @@
+provider "azurerm" {
+  features {}
+
+  subscription_id = "8f868a9e-024e-4dde-b50a-13f965901fab"
+  client_id       = "3be863c2-efae-48a0-8f76-1b6d15f469f2"
+  client_secret   = "gGPDTCUI~.tz85zQdty9_5AxXQHS9XKAF_"
+  tenant_id       = "2d83fc21-e4aa-4b58-9572-825f31b6241e"
+}
