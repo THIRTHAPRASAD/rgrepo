139983500408aff9dd4565e0cf86e449f9c18403
THIRTHAPRASAD <prasadprabha19@gmail.com>
2021-03-08 13:50:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "example1" {
   name     = "rg2"
   location = "West Europe"
 }
 
 resource "azurerm_storage_account" "example" {
   name                     = "sa2"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+  resource_group_name      = azurerm_resource_group.example1.name
+  location                 = azurerm_resource_group.example1.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 
